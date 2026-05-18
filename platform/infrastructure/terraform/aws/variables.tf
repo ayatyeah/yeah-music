ae0f156ed69857e21c3dff508615fc22674b88d6
ayatyeah <balmagambetaat@gmail.com>
2026-05-19 01:50:33 +0500

    terraform upd

--- platform/infrastructure/terraform/aws/variables.tf
+++ platform/infrastructure/terraform/aws/variables.tf
@@ -5,7 +5,6 @@ variable "aws_region" {
 
 variable "ami_id" {
   type        = string
-  description = "Ubuntu/Debian AMI id"
 }
 
 variable "instance_type" {
@@ -15,18 +14,13 @@ variable "instance_type" {
 
 variable "ssh_key_name" {
   type        = string
-  description = "Existing EC2 key pair name"
 }
 
 variable "ssh_cidr" {
   type        = string
-  description = "Your IP address for SSH access (e.g., 192.168.1.100/32)"
-  default     = "0.0.0.0/0"
 }
 
 variable "demo_cidr" {
   type        = string
-  description = "Your IP address for demo ports (e.g., 192.168.1.100/32)"
-  default     = "0.0.0.0/0"
 }
 
